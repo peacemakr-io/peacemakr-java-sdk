a753b58ad31ca1007ab8d81c479773431c379180
interstellarPotato <encrypteddryfruit@gmail.com>
2019-06-18 21:02:24 -0700

    Do not include travis

--- build_sdk.sh
+++ build_sdk.sh
@@ -5,4 +5,4 @@ set -ex
 rm -rf ./src/main/java/io/swagger/client/ || true
 rm -rf ./src/test/java/io/swagger/client/ || true
 ./generate-stub-client.sh
-./gradlew clean build
+./gradlew clean build --info
