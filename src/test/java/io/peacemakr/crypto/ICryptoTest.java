15a9added4a6544c26efc79447466e8d90b87375
Jon McLachlan <jon.mclachlan@icloud.com>
2019-06-22 13:22:19 -0700

    Add in tests for the main interface

---
+++ src/test/java/io/peacemakr/crypto/ICryptoTest.java
@@ -0,0 +1,54 @@
+package io.peacemakr.crypto;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ICryptoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void register() {
+    }
+
+    @Test
+    public void sync() {
+    }
+
+    @Test
+    public void encrypt() {
+    }
+
+    @Test
+    public void encrypt1() {
+    }
+
+    @Test
+    public void encryptInDomain() {
+    }
+
+    @Test
+    public void encryptInDomain1() {
+    }
+
+    @Test
+    public void decrypt() {
+    }
+
+    @Test
+    public void decrypt1() {
+    }
+
+    @Test
+    public void getDebugInfo() {
+    }
+}
