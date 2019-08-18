377bf61ee52d0ef05f2cd9dd499b24a7aa2aece9
Jon McLachlan <jon.mclachlan@icloud.com>
2019-08-18 16:43:43 -0700

    Move client code generation into our model

--- generate-stub-client.sh
+++ generate-stub-client.sh
@@ -5,4 +5,5 @@ set -ex
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli:2.4.5 generate \
     -i local/peacemakr-services.yml \
     -l java \
-    -o local
+    -o local \
+	--api-package io.peacemakr.crypto.swagger.client.api
