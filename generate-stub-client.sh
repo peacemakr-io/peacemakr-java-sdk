ff436ab953a42fb6a86a7fed151f70247a25c703
Jon McLachlan <jon.mclachlan@icloud.com>
2019-08-18 16:55:04 -0700

    Generate peacemakr stub client within our own space

--- generate-stub-client.sh
+++ generate-stub-client.sh
@@ -2,8 +2,12 @@
 
 set -ex
 
+rm -rf src/main/java/io/peacemakr/crypto/swagger || true
+rm -rf src/test/java/io/peacemakr/crypto/swagger || true
+
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli:2.4.5 generate \
     -i local/peacemakr-services.yml \
     -l java \
     -o local \
-	--api-package io.peacemakr.crypto.swagger.client.api
+    --api-package io.peacemakr.crypto.swagger.client.api \
+    --model-package io.peacemakr.crypto.swagger.client.model
