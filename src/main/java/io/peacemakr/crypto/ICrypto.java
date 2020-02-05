19cca62ad436db7ce435eb4bc81d9868e8778d08
interstellarPotato <sasha.sinkevich@gmail.com>
2020-02-04 22:32:54 -0800

    Add a release mechanism to push jar to the Central

--- src/main/java/io/peacemakr/crypto/ICrypto.java
+++ src/main/java/io/peacemakr/crypto/ICrypto.java
@@ -13,8 +13,8 @@ public interface ICrypto {
    * to call multiple times. Once a successful invocation of Register is executed once, subsequent calls become a
    * noop. One successful call is required before any cryptographic use of this SDK.
    * <p>
-   * Registration may fail with invalid apiKey, missing network connectivity, or an invalid persister. On failure,
-   * take corrections action and invoke again.
+   * @throws PeacemakrException if an exception occurs during the registration. Registration may fail with invalid
+   * apiKey, missing network connectivity, or an invalid persister. On failure, take corrections action and invoke again.
    */
   void register() throws PeacemakrException;
 
@@ -22,14 +22,14 @@ public interface ICrypto {
    * Sync all available keys for this client. This invocation will help performance of subsequent encryption
    * and decryption calls.
    * <p>
-   * Sync may fail, if registration was not invoked, if there's network connectivity issues, or
-   * unexpected authorization issues.
+   * @throws PeacemakrException if an exception occurs during the sync. Sync may fail, if registration was not invoked,
+   * if there's network connectivity issues, or unexpected authorization issues.
    */
   void sync() throws PeacemakrException;
 
   /**
    * Encrypt the plaintext, using a random available usedomain.
-   *
+   * <p>
    * @param plainText Plaintext bytes to encrypt.
    * @return Opaquely packaged ciphertext.
    * @throws PeacemakrException On any error (network connectivity issues, authN issues, etc)
@@ -43,22 +43,28 @@ public interface ICrypto {
    * Use Domain with the same name. The transitional purity, both Use Domains may be selected for encryption use by
    * clients restricted to one particular name. Then, retiring of one of the two Use Domains is possible without
    * disrupting your deployed application.
-   *
+   * <p>
    * @param plainText     Plaintext to encrypt.
    * @param useDomainName Non-unique User Domain of your organization's.
+   * @return Opaquely packaged ciphertext.
+   * @throws PeacemakrException if an exception occurs during the encryption.
+   * @throws UnsupportedEncodingException if unsupported encoding has been used.
    */
   byte[] encryptInDomain(byte[] plainText, String useDomainName) throws PeacemakrException, UnsupportedEncodingException;
 
   /**
    * Decrypt the opaquely packaged ciphertext and return the original plain text.
-   *
+   * <p>
    * @param cipherText CipherText to decrypt.
+   * @return original plain text.
+   * @throws PeacemakrException if an exception occurs during the decryption.
    */
   byte[] decrypt(byte[] cipherText) throws PeacemakrException;
 
   /**
    * For visibility or debugging purposes, returns a string whihc identifies which
    * client and configuration this client is running.
+   * @return a debug info.
    */
   String getDebugInfo();
 }
