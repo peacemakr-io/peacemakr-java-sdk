dcba790a45ded68a76bca58700a43ed36800ada0
interstellarPotato <sasha.sinkevich@gmail.com>
2020-02-07 16:38:29 -0800

    do not sign nor push by default when building the project

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ set -ex
 rm -rf ./src/main/java/io/swagger/client/ || true
 rm -rf ./src/test/java/io/swagger/client/ || true
 ./generate-stub-client.sh
-./gradlew clean build --info
+./gradlew clean build --info -x uploadArchives
